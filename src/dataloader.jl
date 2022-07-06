f5143ad7aebe51c6ee326c3fba20bf69b7ae62d0
kjxymful <pleibersperger@yahoo.de>
2022-07-06 18:26:04 +0200

    Cosmetic Fix

--- src/dataloader.jl
+++ src/dataloader.jl
@@ -1,8 +1,8 @@
-using JLD2: load
+import JLD2: load as jld2load
 using BPTT
 
 function load_data(path::String, name::String; device = cpu)
-    data_dict = load(path)
+    data_dict = jld2load(path)
     X = Float32.(data_dict["time series"]) |> device
     if "par" in keys(data_dict)
         par = data_dict["par"]
