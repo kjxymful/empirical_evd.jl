a84cd72044cc52cc1fd3c47f25bc42e36b835fc1
kjxymful <pleibersperger@yahoo.de>
2022-07-07 12:20:26 +0200

    Move dataloader

--- src/utils.jl
+++ src/utils.jl
@@ -1,10 +1,11 @@
 using BSON: @load
+import JLD2: load as jld2load
 using BPTT
 
 function load_model(exp::String, run::Int, epoch::Int; eval=true)::BPTT.AbstractPLRNN
     load_path = joinpath(["Results", exp, format_run_ID(run)])
     if eval
-        load_path = "../"*load_path
+        load_path = "../" * load_path
     end
     @load joinpath(load_path, "checkpoints", "model_$epoch.bson") m
     return m
@@ -16,4 +17,16 @@ function format_run_ID(run::Int)::String
     @assert n_digits < 4
     add_zeros = 3 - n_digits
     return repeat("0", add_zeros) * run_str
+end
+
+function load_data(path::String, name::String; device=cpu)
+    data_dict = jld2load(path)
+    X = Float32.(data_dict["time series"]) |> device
+    if "par" in keys(data_dict)
+        par = data_dict["par"]
+    else
+        par = nothing
+    end
+
+    return BPTT.Dataset(X, name), par
 end
